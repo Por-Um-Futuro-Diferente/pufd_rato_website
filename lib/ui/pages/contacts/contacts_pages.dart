59b406b62fa49cf2c3cd990917f3ef70d25204b3
mxhrxbxn <mehribandedegil@gmail.com>
2022-08-12 16:17:46 +0100

    created structure for the project

---
+++ lib/ui/pages/contacts/contacts_pages.dart
@@ -0,0 +1,49 @@
+
+import 'package:flutter/material.dart';
+
+class ContactsPage extends StatefulWidget {
+  const ContactsPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<ContactsPage> createState() => _ContactsPageState();
+}
+
+class _ContactsPageState extends State<ContactsPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
